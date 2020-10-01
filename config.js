972e9524d9c402c82fc5aaad2968e3e9570d3574
Jonah04 <yonast.yilma@gmail.com>
2020-10-01 16:29:46 +0300

    Working

---
+++ config.js
@@ -0,0 +1,7 @@
+const TOKEN = "1278323065:AAHY-pWkeqIT2ht_2InM0u2ZElZJ1eyMVbo";
+const PORT = process.env.PORT || 8080;
+const ADMIN_ID = 288870772;
+// const ADMIN_ID = -1001480243428;
+module.exports.TOKEN = TOKEN;
+module.exports.PORT = PORT;
+module.exports.ADMIN_ID = ADMIN_ID;
---
+++ index.js
@@ -0,0 +1,104 @@
+var config = require('./config.js');
+var express = require('express');
+const Bot = require('node-telegram-bot-api');
+var http = require('https');
+var path = require('path');
+const { stat } = require('fs');
+var level = require('level')
+var db = level('my-db')
+var app = express();
+const bot = new Bot(config.TOKEN, { polling: true });
+
+let states = ['init', 'name', 'phone_no', 'photo', 'done'];
+
+let user = {
+    id: Math.floor(Math.random() * 90000) + 10000,
+    chat_id: '',
+    fullname: '',
+    phonenumber: '',
+    lottonumber: '',
+    state: states[0],
+    status: ''
+}
+
+bot.onText(/\/start/, (msg) => {
+
+    bot.sendMessage(msg.chat.id, "Please Send Me Your Full Name");
+    user.chat_id = msg.chat.id;
+    db.put(user.id, JSON.stringify(user), function(err) {
+        user.state = states[1];
+        if (err) return console.log('Err', err)
+    });
+});
+
+bot.on('message', (msg) => {
+
+    if (user.state == 'name') {
+        bot.sendMessage(msg.chat.id, "Your Phone Number:");
+        user.fullname = msg.text;
+        user.state = states[2];
+    } else if (user.state == 'phone_no') {
+        bot.sendMessage(msg.chat.id, "Send Me The Reciept of your Payment:");
+        user.phoneNumber = msg.text;
+        user.state = states[3];
+
+    } else if (user.state == 'photo') {
+
+
+        user.state = states[4];
+
+        var options = {
+            reply_markup: JSON.stringify({
+                inline_keyboard: [
+                    [{
+                        text: 'Approve',
+                        callback_data: '1'
+                    }],
+                    [{
+                        text: 'Decline',
+                        callback_data: '2'
+                    }],
+                ]
+            })
+        };
+        db.put(user.id, JSON.stringify(user), function(err) {
+            if (err) return console.log('Err', err)
+        });
+        bot.sendMessage(msg.chat.id, "Thank You for your participation 😄! we will check and send you your lottery number");
+        bot.forwardMessage(config.ADMIN_ID, msg.from.id, msg.message_id);
+        bot.sendMessage(config.ADMIN_ID, "Is the reciept valid", options);
+    }
+
+});
+bot.on("polling_error", (err) => console.log(err));
+bot.on('callback_query', function onCallbackQuery(callbackQuery) {
+    const action = callbackQuery.data;
+    const msg = callbackQuery.message;
+    let user_data;
+    db.get(user.id, function(err, value) {
+        if (err) return console.log('Ooops!', err)
+        user_data = JSON.parse(value);
+        if (action === '1') {
+            user.lottonumber = Math.floor(Math.random() * 9000) + 1000;
+            bot.sendMessage(user_data.chat_id, `
+                Dear ${ user_data.fullname }
+
+                Yout Lotto Number is ${ user.lottonumber }
+
+                The Winners will be announced on 10/10/20!
+                Stay tuned on Our channels and groups
+            `);
+        } else {
+            bot.sendMessage(user_data.chat_id, 'You are denied');
+        }
+    })
+
+});
+
+
+
+
+
+app.listen(config.PORT, function() {
+    console.log('Our app is running on http://localhost:' + config.PORT);
+});
